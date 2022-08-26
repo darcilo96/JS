352da725610bb67746c306185e253fcf057f396b
David Arcila Flórez <david.arcila96@gmail.com>
2022-08-26 15:58:38 -0500

    Se aplicó ejemplo de ciclo for y while

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-let nombre= prompt("Ingrese su nombre");
-let apellido= prompt("Ingrese sus apellidos");
+/* Ingrese la tasa en porcentaje (ejemplo: 30% ===> 30) */
+let tasa=parseInt(prompt("Ingrese la tasa de rentabilidad (ejemplo: 30% ===> 30)"));
+/* Ingrese el monto a invertir (USD) */
+let monto=parseInt(prompt("Ingrese el monto a invertir (USD)"));
+/* Ingrese el tiempo de inversión(meses) */
+let tiempo=parseInt(prompt("Ingrese el tiempo de inversión (en meses)"));
 
-let nombreCompleto= nombre + " "+ apellido;
-
-alert(nombreCompleto);
-console.log(nombreCompleto);
+/*Se realiza la iteración para mostrar el saldo mensual*/
+for(let i=0; i<=tiempo; i++){
+/*Se aplica la fórmula del interés compuesto */
+let rentabilidad=(1+tasa/100)**(i);
+let resultado= monto*(rentabilidad);
+/* Se muestra el seguimiento de cada mes */
+alert(`Mes ${i}: ${resultado.toFixed(2)} USD`);
+/* Pregunta si desea continuar invirtiendo */
+let confirmacion= prompt("¿Desea continuar invirtiendo?")
+while(confirmacion!=="No"){
+    alert("Tu dinero sigue trabajando por ti");
+    break;
+}
+/* Corta el ciclo en caso de que el usuario no desee continuar invirtiendo */
+if(confirmacion==="no"){
+    alert("Fue un placer servirte");
+    break;
+}
+}
